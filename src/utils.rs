5851b7127e6a0c41048cb2a69fb2b4480fcc3a18
Kim D. Jeker <git@kije.ch>
2023-09-20 16:47:21 +0200

    added get_factor_column_name() util

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,7 @@
+use std::borrow::Cow;
 use polars::export::num::FromPrimitive;
 use polars::prelude::*;
+use crate::{FactorColumn};
 
 pub fn convert_time_units(v: i64, tu_l: TimeUnit, tu_r: TimeUnit) -> i64 {
     match (tu_l, tu_r) {
@@ -20,6 +22,7 @@ pub struct ValueRange {
     pub min: Option<f32>,
     pub max: Option<f32>,
 }
+
 pub fn map_sleep_duration_value_to_value_range(sleep: f32) -> Option<ValueRange> {
     match i32::from_f32(sleep)? {
         190 => Some(ValueRange {
@@ -54,4 +57,13 @@ pub fn map_sleep_duration_value_to_value_range(sleep: f32) -> Option<ValueRange>
         }),
         _ => None,
     }
+}
+
+pub fn get_factor_column_name(factors: Vec<FactorColumn>, factor_tag: &str, factor_type_tag: &str) -> Option<Cow<'_, str>> {
+    factors
+        .iter()
+        .find(|factor_column| {
+            *factor_column.factor.tag() == factor_tag && *factor_column.factor_type.tag() == factor_type_tag
+        })
+        .map(|factor_column| factor_column.column_name.clone())
 }
